3afb21460b7bdd6edbe0b2bb35d34d363eeb535d
Abdul Rafay <rafayzia3690@gmail.com>
2024-01-26 12:30:30 +0500

    code added

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/AbdulRafayZia/tor
+
+go 1.21.6
---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+// TorRelay represents a Tor relay server.
+type TorRelay struct {
+	Address string
+}
+
+// TorBrowserSimulator simulates the behavior of a Tor Browser.
+type TorBrowserSimulator struct {
+	Relays []*TorRelay
+}
+
+// NewTorBrowserSimulator creates a new Tor Browser Simulator with the given relays.
+func NewTorBrowserSimulator(relays []*TorRelay) *TorBrowserSimulator {
+	return &TorBrowserSimulator{
+		Relays: relays,
+	}
+}
+
+// sendRequestThroughRelay sends an HTTP request through a Tor relay.
+func (tbs *TorBrowserSimulator) sendRequestThroughRelay(relay *TorRelay, targetURL string) ([]byte, error) {
+	// Configure a proxy URL to use the Tor relay.
+	proxyURL, err := url.Parse("http://" + relay.Address)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create an HTTP client with the proxy configuration.
+	client := &http.Client{
+		Transport: &http.Transport{
+			Proxy: http.ProxyURL(proxyURL),
+		},
+		Timeout: time.Second * 10,
+	}
+
+	// Send an HTTP GET request through the Tor relay.
+	resp, err := client.Get(targetURL)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	// Read the response body.
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
+
+func main() {
+	// Set up Tor relays.
+	relay1 := &TorRelay{Address: "localhost:9001"}
+	relay2 := &TorRelay{Address: "localhost:9002"}
+	relays := []*TorRelay{relay1, relay2}
+
+	// Create Tor Browser Simulator.
+	tbs := NewTorBrowserSimulator(relays)
+
+	// Specify the target URL.
+	targetURL := "https://www.nasa.gov/"
+
+	// Simulate sending a request through a random Tor relay.
+	selectedRelay := relays[0] // You can implement logic to select a random relay.
+	response, err := tbs.sendRequestThroughRelay(selectedRelay, targetURL)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	// Print the response.
+	fmt.Println("Response from Tor relay:", string(response))
+}
